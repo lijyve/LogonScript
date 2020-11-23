c6f5dabe8d23fe9b47952fcdc5e2fdd101f70ee3
Virtual-Y-Monster <1113459606@qq.com>
2020-11-23 16:06:51 +0800

    更新签到接口

--- Script/shandian.js
+++ Script/shandian.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name              闪电加速签到
 // @namespace         https://soulsign.inu1255.cn?account=Virtual-Y-Monster
-// @version           1.0.1
+// @version           1.0.2
 // @author            Virtual-Y-Monster
-// @loginURL          https://freemycloud.pw/auth/login
+// @loginURL          https://freemycloud.me/auth/login
 // @updateURL         https://soulsign.inu1255.cn/script/Virtual-Y-Monster/闪电加速签到
 // @expire            900000
-// @domain            freemycloud.pw
+// @domain            freemycloud.me
 // @param            email 邮箱
 // @param            passwd 密码
 // ==/UserScript==
@@ -14,7 +14,7 @@
 exports.run = async function (param) {
     var {
         data
-    } = await axios.post('https://freemycloud.pw/auth/login', {
+    } = await axios.post('https://freemycloud.me/auth/login', {
         'email': param.email,
         'passwd': param.passwd,
         'remember_me': 'on'
@@ -24,21 +24,21 @@ exports.run = async function (param) {
     }
     var {
         data
-    } = await axios.post('https://freemycloud.pw/user/checkin')
+    } = await axios.post('https://freemycloud.me/user/checkin')
     return data.msg;
 };
 
 exports.check = async function (param) {
     var {
         data
-    } = await axios.post('https://freemycloud.pw/auth/login', {
+    } = await axios.post('https://freemycloud.me/auth/login', {
         'email': param.email,
         'passwd': param.passwd,
         'remember_me': 'on'
     });
     var {
         data
-    } = await axios.get('https://freemycloud.pw/user');
+    } = await axios.get('https://freemycloud.me/user');
     if (/用户中心 — 闪电/.test(data)) {
         return true;
     } else {
