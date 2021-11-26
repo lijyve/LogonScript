9cb4571a3d7bdbc57bb45eafcfd0e0746a7ec058
Virtual-Y-Monster <1113459606@qq.com>
2021-11-26 10:19:11 +0800

    添加力扣中国签到脚本

--- Script/leetcode-cn.js
+++ Script/leetcode-cn.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              力扣中国
 // @namespace         https://soulsign.inu1255.cn?account=Virtual-Y-Monster
-// @version           1.0.0
+// @version           1.0.1
 // @author            Virtual-Y-Monster
 // @loginURL          https://leetcode-cn.com/
 // @expire            900000
@@ -9,40 +9,27 @@
 // ==/UserScript==
 
 exports.run = async function (param) {
-    var { data } = await axios.post('https://leetcode-cn.com/graphql/noj-go',
-        {
-            "operationName": "userStatusGlobal",
-            "variables": {},
-            "query": "query userStatusGlobal {\n  userStatus {\n    isSignedIn\n    isAdmin\n    isStaff\n    isSuperuser\n    isTranslator\n    isVerified\n    isPhoneVerified\n    isWechatVerified\n    checkedInToday\n    username\n    realName\n    userSlug\n    groups\n    avatar\n    optedIn\n    requestRegion\n    region\n    socketToken\n    activeSessionId\n    permissions\n    completedFeatureGuides\n    useTranslation\n    accountStatus {\n      isFrozen\n      inactiveAfter\n      __typename\n    }\n    __typename\n  }\n}\n"
-        });
-    if (!data.data.userStatus.isSignedIn)
-        throw "没有登录";
-    if (data.data.userStatus.checkedInToday)
-        return "重复签到";
-    var { data } = await axios.get('https://leetcode-cn.com/points/api/total/');
-    var originPoints = data.points;
+    var defaultHeaders = { "Origin": "https://leetcode-cn.com/", "Referer": "https://leetcode-cn.com/points/" };
     var { data } = await axios.post('https://leetcode-cn.com/graphql/',
         {
             "operationName": "checkin",
             "variables": {},
             "query": "mutation checkin {\n  checkin {\n    checkedIn\n    ok\n    error\n    __typename\n  }\n}\n"
-        },
-        {
-            headers: { "Origin": "https://leetcode-cn.com/", "Referer": "https://leetcode-cn.com/points/" }
-        });
-    if (!data.data.checkin.checkedIn)
-        throw data.data.checkin.error;
-    var { data } = await axios.get('https://leetcode-cn.com/points/api/total/');
-    var getPoints = data.points - originPoints;
-    return "今日获得 " + getPoints + " 积分";
+        }, { headers: defaultHeaders });
+    data = data.data;
+    if (data.checkin === null)
+        throw '未登录';
+    else if (!data.checkin.checkedIn && data.checkin.ok)
+        return '重复签到';
+    else if (data.checkin.checkedIn && data.checkin.ok)
+        return '签到成功';
+    else
+        throw '签到失败';
 };
 
 exports.check = async function (param) {
-    var { data } = await axios.post('https://leetcode-cn.com/graphql/noj-go',
-        {
-            "operationName": "userStatusGlobal",
-            "variables": {},
-            "query": "query userStatusGlobal {\n  userStatus {\n    isSignedIn\n    isAdmin\n    isStaff\n    isSuperuser\n    isTranslator\n    isVerified\n    isPhoneVerified\n    isWechatVerified\n    checkedInToday\n    username\n    realName\n    userSlug\n    groups\n    avatar\n    optedIn\n    requestRegion\n    region\n    socketToken\n    activeSessionId\n    permissions\n    completedFeatureGuides\n    useTranslation\n    accountStatus {\n      isFrozen\n      inactiveAfter\n      __typename\n    }\n    __typename\n  }\n}\n"
-        });
-    return data.data.userStatus.isSignedIn;
+    var { data } = await axios.get('https://leetcode-cn.com/points/api/total/');
+    if ('detail' in data)
+        return !/身份认证信息未提供。/.test(data.detail);
+    return true;
 };
